bf10ca4b7dcba438963e863762c34bf6f0ac3530
Joosep <joosepliiv97@gmail.com>
2018-03-12 11:54:32 +0200

    First commit

---
+++ plugins/octo/timeline/lang/en/lang.php
@@ -0,0 +1,38 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Timeline',
+        'description' => 'Simple plugin to write and display TimeLine based events on Your website.'
+    ],
+    'form' => [
+        'title' => 'Entry Title',
+        'title_desc' => 'Keep it short and simple.',
+        'title_placeholder' => 'My new event!',
+        'text' => 'Entry Text',
+        'text_description' => 'Here you post an event.',
+        'text_placeholder' => 'Describe something amazing for your visitors!',
+        'public' => 'Public Post',
+        'public_desc' => 'If enabled, post will be visible to all visitors. If disabled - only post author.',
+        'date' => 'Start Date',
+        'end_date' => 'End Date',
+        'time' => 'Event Time',
+        'create' => 'Create Entry',
+        'update' => 'Update Entry',
+        'saved' => 'Entry Saved',
+        'deleted' => 'Entry Deleted',
+        'preview' => 'Entry Preview',
+        'color' => 'Color',
+    ],
+    'list' => [
+        'id' => 'ID',
+        'title' => 'Title',
+        'date' => 'Start Date',
+        'end_date' => 'End Date',
+        'public' => 'Public',
+        'yes' => 'Yes',
+        'no' => 'No',
+    ],
+    'menu' => [
+        'main' => 'Timeline',
+        'side_entries' => 'Entries',
+    ],
+];
---
+++ themes/demo/assets/js/custom.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+    // Otsime välja paragrahvid, mille class on col-list
+    var x = document.getElementsByClassName("col-list");
+    var i;
+    // Loopime välja leitud paragrahvid
+    for (i = 0; i < x.length; i++) {
+        var p = x[i];
+        // Salvestame paragrahvi algse väärtuse
+        var headline = $(p).text();
+        // Listi collapse nupu väärtus ja link
+        $(p).html('<a class="btn btn-info" data-toggle="collapse" href="#' + headline + '">' + headline.toUpperCase() + '</a>');
+        // Püüame kinni paragrahvile järgneva elemendi ( ul, millest saab collapsable listi sisu ) 
+        var content = x[i].nextElementSibling;
+        content.outerhtml = '<span>';
+        // `element` is the element you want to wrap
+        var parent = content.parentNode;
+        // Loome divi contenti ümber
+        var wrapper = document.createElement('div');
+        $(wrapper).attr({class:"collapse", id: headline});
+        // set the wrapper as child (instead of the element)
+        parent.replaceChild(wrapper, content);
+        // set element as child of wrapper
+        wrapper.appendChild(content);
+    }
+});
